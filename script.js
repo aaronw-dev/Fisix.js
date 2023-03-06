c2db28c48dafec874c3f16b732546a3fb7c20173
AW_Dev <riptidegamesceo@gmail.com>
2023-03-06 12:22:06 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import {FourPointShape} from "collidershape.js";
-
 var topRight;
 var topLeft;
 var bottomRight;
@@ -35,60 +33,104 @@ var bounce = 1;
 var isgrabbed = false;
 var simulate = false;
 
+//Canvas height variables
+var canvasW;
+var canvasH;
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
 //Set initial position of ball
 ball.style.top = targetY + "%";
 ball.style.left = targetX + "%";
 
 //Update every "frame" at 100 FPS
 setInterval(Update, 10);
-
-var shape = new FourPointShape([0,0],[100,0],[100,100],[0,100]);
-function Update() {
-    TickGravity();
-    TickCollision(shape);
-    UpdateVisuals();
+InitCanvas();
+class Vector2 {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 }
+class CollisionLine {
+  constructor(position1, position2) {
+    this.position1 = position1;
+    this.position2 = position2;
 
-function TickGravity() {
-    if (simulate) {
-        velocityY -= gravityY * ballMass;
-        velocityX -= gravityX * ballMass;
-        positionY -= velocityY;
-        positionX -= velocityX;
-    }
+    ctx.beginPath();
+    ctx.moveTo(position1.x, position1.y);
+    ctx.lineTo(position2.x, position2.y);
+    ctx.stroke()
+  }
 }
+class FourPointShape {
+  constructor(topLeft, topRight, bottomRight, bottomLeft, color) {
+    this.topLeft = topLeft;
+    this.topRight = topRight;
+    this.bottomLeft = bottomLeft;
+    this.bottomRight = bottomRight;
+    this.color = color;
 
-function TickCollision(shape) {
-  if(positionX < shape.topRight[0] || positionX > shape.topLeft){
-    console.log("True");
+    var topLine = new CollisionLine(new Vector2(topLeft[0], topLeft[1]), new Vector2(topRight[0], topRight[1]))
+    var rightLine = new CollisionLine(new Vector2(topRight[0], topRight[1]), new Vector2(bottomRight[0], bottomRight[1]))
+    var bottomLine = new CollisionLine(new Vector2(bottomRight[0], bottomRight[1]), new Vector2(bottomLeft[0], bottomLeft[1]))
+    var leftLine = new CollisionLine(new Vector2(bottomLeft[0], bottomLeft[1]), new Vector2(topLeft[0], topLeft[1]))
   }
 }
 
+var shape = new FourPointShape([0, 0], [300, 0], [300, 300], [0, 300]);
+
+function InitCanvas() {
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+  canvasW = canvas.width;
+  canvasH = canvas.height;
+  ctx.lineWidth = 2;
+}
+
+function Update() {
+  TickGravity();
+  TickCollision(shape);
+  UpdateVisuals();
+}
+
+function TickGravity() {
+  if (simulate) {
+    velocityY -= gravityY * ballMass;
+    velocityX -= gravityX * ballMass;
+    positionY -= velocityY;
+    positionX -= velocityX;
+  }
+}
+
+function TickCollision(shape) {}
+
 function UpdateVisuals() {
-    ball.style.top = positionY + "px";
-    ball.style.left = positionX + "px";
+  ball.style.top = positionY + "px";
+  ball.style.left = positionX + "px";
 }
 
 function SetZeroPosition() {
-    velocityY = 0;
-    velocityX = 0;
-    positionY = 0;
-    positionX = 50;
+  velocityY = 0;
+  velocityX = 0;
+  positionY = 0;
+  positionX = 50;
 }
 
 document.onclick = (e) => {
-    velocityY = 0;
-    velocityX = 0;
-    positionY = e.y;
-    positionX = e.x;
+  velocityY = 0;
+  velocityX = 0;
+  positionY = e.y;
+  positionX = e.x;
 };
 
 function LogOut(output, delay = 100) {
-    let box = document.getElementById("output");
-    var msg = document.createElement("div");
-    msg.innerHTML = output;
-    box.appendChild(msg);
-    setTimeout(() => {
-        box.removeChild(msg);
-    }, delay);
+  let box = document.getElementById("output");
+  var msg = document.createElement("div");
+  msg.innerHTML = output;
+  box.appendChild(msg);
+  setTimeout(() => {
+    box.removeChild(msg);
+  }, delay);
 }
