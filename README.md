ea0138ea05fd076818333557bf567d605bf6cf4e
AW_Dev <riptidegamesceo@gmail.com>
2023-03-09 22:04:34 +0000

    forogor again

--- README.md
+++ README.md
@@ -3,9 +3,14 @@
 
 A vanilla JavaScript-based spring simulation. Change parameters in ```script.js```. Will implement a GUI soon.
 
+## Links
+- [How to get the nearest point outside a polygon from a point inside a polygon](https://stackoverflow.com/questions/19048122/how-to-get-the-nearest-point-outside-a-polygon-from-a-point-inside-a-polygon)  
+- [Shortest distance between a point and a line segment](https://stackoverflow.com/questions/849211/shortest-distance-between-a-point-and-a-line-segment)
+
 ## To Do
 - [ ] Add velocity reflection on contact
 - [ ] Implement "closest point on polygon" algorithm
 
 ## Development Log
-**3/8/2023 11:20AM:** Rewrote physics engine using a [physics theory video by Gonkee](youtu.be/kyQP4t_wOGI)
+**3/8/2023 11:20AM** Rewrote physics engine using a [physics theory video by Gonkee](youtu.be/kyQP4t_wOGI)
+**3/9/2022 1:30PM** Added functionality to draw lines between balls
--- script.js
+++ script.js
@@ -1,164 +1,158 @@
 var massPoints = [];
 var collisionPolys = [];
+var springs = [];
+numbballs = 0
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const linecanvas = document.getElementById("linecanvas");
 const linectx = linecanvas.getContext("2d");
-ctx.canvas.width  = window.innerWidth;
+ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
-linectx.canvas.width  = window.innerWidth;
+linectx.canvas.width = window.innerWidth;
 linectx.canvas.height = window.innerHeight;
 
-var gravity = [0,-0.1];
+var gravity = [0, -0.1];
 
-class MassPoint{
-  constructor(positionX, positionY, bounce, mass){
-    this.positionX = positionX;
-    this.positionY = positionY;
-    this.mass = mass;
-    this.bounce = bounce;
-    this.velocityY = 0;
-    this.velocityX = 0;
-    
-    let visual = document.createElement('div');
-    visual.className = "point";
-    document.getElementById("masspoints").appendChild(visual);
-    visual.style.top = positionY + "px";
-    visual.style.left = positionX + "px";
-    
-    this.visual = visual;
-  }
-}
-class Spring{
-  constructor(massPointA, massPointB, restLength, stiffness = 1, dampingFactor = 1){
-    this.massPointA = massPointA;
-    this.massPointB = massPointB;
-    this.restLength = restLength;
-    this.stiffness = stiffness;
-    this.dampingFactor = dampingFactor;
-  }
-}
-class Polygon{
-  constructor(points){
-    this.points = points;
-  }
-}
-function InitMassPoint(positionX,positionY,bounce = 1,mass=1){
-  var point = new MassPoint(positionX, positionY, bounce,mass)
-  massPoints.push(point);
-  return point
-}
+class MassPoint {
+    constructor(positionX, positionY, bounce, mass) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.mass = mass;
+        this.bounce = bounce;
+        this.velocityY = 0;
+        this.velocityX = 0;
 
-function InitPolygon(points,width=2){
-  collisionPolys.push(new Polygon(points));
-  DrawPolygon(points,width);
-}
-function DisplayMassPoints(){
-  for(let i = 0; i < massPoints.length; i++){
-    massPoints[i].visual.style.top = massPoints[i].positionY + "px";
-    massPoints[i].visual.style.left = massPoints[i].positionX + "px";
-  }
-  
-  linectx.clearRect(0, 0, canvas.width, canvas.height);
-  DrawLine([[massPoints[0].positionX, massPoints[0].positionY], [massPoints[1].positionX,massPoints[1].positionY]],3,"#FF0000")
-  DrawLine([[massPoints[1].positionX, massPoints[1].positionY], [massPoints[2].positionX,massPoints[2].positionY]],3,"#FF0000")
-}
+        let visual = document.createElement('div');
+        visual.className = "point";
+        document.getElementById("masspoints").appendChild(visual);
+        visual.style.top = positionY + "px";
+        visual.style.left = positionX + "px";
 
-function TickGravity(){
-  for(let i = 0; i < massPoints.length; i++){
-    var point = massPoints[i];
-    var force = [0,0];
-    force[0] += gravity[0] * point.mass;
-    force[1] += gravity[1] * point.mass;
-    var velocity = [point.velocityX,point.velocityY];
-    velocity[0] += force[0] / point.mass;
-    velocity[1] += force[1] / point.mass;
-    
-    massPoints[i].velocityX = velocity[0];
-    massPoints[i].velocityY = velocity[1];
-    
-    massPoints[i].positionX -= massPoints[i].velocityX;
-    massPoints[i].positionY -= massPoints[i].velocityY;
-    var isIntersecting = pointInPolygon(collisionPolys[0].points, [massPoints[i].positionX, massPoints[i].positionY])
-    if(isIntersecting){
-      /*Calculate reflections
-        Reflection in a nutshell:
-        w = v - 2 * (v ∙ n) * n
-        or
-        reflected vector = input vector - 2 * (input vector * normal) * normal
-      */
-      //var velocityVector = [masspoints[i].velocityX,masspoints[i].velocityY]
-      //var reflectedVector = velocityVector - 2 * (velocityVector * normalVector) * normalVector
-      massPoints[i].velocityY = -massPoints[i].velocityY * point.bounce;
-      massPoints[i].positionY = 700;
+        this.visual = visual;
+        massPoints.push(this)
     }
-    
-    DisplayMassPoints();
-  }
+}
+class Spring {
+    constructor(massPointA, massPointB, restLength, stiffness = 1, dampingFactor = 1) {
+        this.massPointA = massPointA;
+        this.massPointB = massPointB;
+        this.restLength = restLength;
+        this.stiffness = stiffness;
+        this.dampingFactor = dampingFactor;
+        springs.push(this)
+    }
+}
+class Polygon {
+    constructor(points) {
+        this.points = points;
+        collisionPolys.push(this)
+    }
+}
+function InitMassPoint(positionX, positionY, bounce = 1, mass = 1) {
+    numbballs++
+    document.getElementById("ballcount").innerHTML = "Balls: " + numbballs
+    return new MassPoint(positionX, positionY, bounce, mass);
 }
 
-function GetNormal(point1, point2){
-  
+function InitPolygon(points, width = 2) {
+    new Polygon(points)
+    DrawPolygon(points, width);
+}
+function DisplayMassPoints() {
+    for (let i = 0; i < massPoints.length; i++) {
+        massPoints[i].visual.style.top = massPoints[i].positionY + "px";
+        massPoints[i].visual.style.left = massPoints[i].positionX + "px";
+    }
+
+    linectx.clearRect(0, 0, canvas.width, canvas.height);
+    for(let i = 0; i < massPoints.length - 1; i++){
+      DrawLine([[massPoints[i].positionX, massPoints[i].positionY], [massPoints[i + 1].positionX, massPoints[i + 1].positionY]], 3, "#FF0000")
+    }
 }
 
-function DrawPolygon(points, width = 1, color = "#000000"){
-  ctx.lineWidth = width;
-  ctx.beginPath();
-  console.log(points);
-  ctx.moveTo(points[0][0], points[0][1]);
-  for(let i = 1; i < points.length; i++){
-    ctx.lineTo(points[i][0],points[i][1]);
-  }
-  ctx.closePath();
-  ctx.strokeStyle = color;
-  ctx.stroke();
+function TickGravity() {
+    for (let i = 0; i < massPoints.length; i++) {
+        var point = massPoints[i];
+        var force = [0, 0];
+        force[0] += gravity[0] * point.mass;
+        force[1] += gravity[1] * point.mass;
+        var velocity = [point.velocityX, point.velocityY];
+        velocity[0] += force[0] / point.mass;
+        velocity[1] += force[1] / point.mass;
+
+        massPoints[i].velocityX = velocity[0];
+        massPoints[i].velocityY = velocity[1];
+
+        massPoints[i].positionX -= massPoints[i].velocityX;
+        massPoints[i].positionY -= massPoints[i].velocityY;
+        var isIntersecting = pointInPolygon(collisionPolys[0].points, [massPoints[i].positionX, massPoints[i].positionY]);
+        if (isIntersecting) {
+
+            massPoints[i].velocityY = -massPoints[i].velocityY * point.bounce;
+            massPoints[i].positionY = collisionPolys[0].points[0][1];
+        }
+        DisplayMassPoints();
+    }
 }
 
-function DrawLine(points, width = 1, color = "#000000"){
-  linectx.lineWidth = width;
-  linectx.beginPath();
-  linectx.moveTo(points[0][0], points[0][1]);
-  for(let i = 1; i < points.length; i++){
-    linectx.lineTo(points[i][0],points[i][1]);
-  }
-  linectx.strokeStyle = color;
-  linectx.stroke();
+function DrawPolygon(points, width = 1, color = "#000000") {
+    ctx.lineWidth = width;
+    ctx.beginPath();
+    ctx.moveTo(points[0][0], points[0][1]);
+    for (let i = 1; i < points.length; i++) {
+        ctx.lineTo(points[i][0], points[i][1]);
+    }
+    ctx.closePath();
+    ctx.strokeStyle = color;
+    ctx.stroke();
+}
+
+function DrawLine(points, width = 1, color = "#000000") {
+    linectx.lineWidth = width;
+    linectx.beginPath();
+    linectx.moveTo(points[0][0], points[0][1]);
+    for (let i = 1; i < points.length; i++) {
+        linectx.lineTo(points[i][0], points[i][1]);
+    }
+    linectx.strokeStyle = color;
+    linectx.stroke();
 }
 
 /**
  * Performs the even-odd-rule Algorithm (a raycasting algorithm) to find out whether a point is in a given polygon.
  * This runs in O(n) where n is the number of edges of the polygon.
  */
-const pointInPolygon = function(polygon, point) {
-  //A point is in a polygon if a line from the point to infinity crosses the polygon an odd number of times
-  let odd = false;
-  //For each edge (In this case for each point of the polygon and the previous one)
-  for (let i = 0, j = polygon.length - 1; i < polygon.length; i++) {
-    //If a line from the point into infinity crosses this edge
-    if (((polygon[i][1] > point[1]) !== (polygon[j][1] > point[1])) // One point needs to be above, one below our y coordinate
-      // ...and the edge doesn't cross our Y corrdinate before our x coordinate (but between our x coordinate and infinity)
-      && (point[0] < ((polygon[j][0] - polygon[i][0]) * (point[1] - polygon[i][1]) / (polygon[j][1] - polygon[i][1]) + polygon[i][0]))) {
-      // Invert odd
-      odd = !odd;
-    }
-    j = i;
+const pointInPolygon = function (polygon, point) {
+    //A point is in a polygon if a line from the point to infinity crosses the polygon an odd number of times
+    let odd = false;
+    //For each edge (In this case for each point of the polygon and the previous one)
+    for (let i = 0, j = polygon.length - 1; i < polygon.length; i++) {
+        //If a line from the point into infinity crosses this edge
+        if (((polygon[i][1] > point[1]) !== (polygon[j][1] > point[1])) // One point needs to be above, one below our y coordinate
+            // ...and the edge doesn't cross our Y corrdinate before our x coordinate (but between our x coordinate and infinity)
+            && (point[0] < ((polygon[j][0] - polygon[i][0]) * (point[1] - polygon[i][1]) / (polygon[j][1] - polygon[i][1]) + polygon[i][0]))) {
+            // Invert odd
+            odd = !odd;
+        }
+        j = i;
 
-  }
-  //If the number of crossings was odd, the point is in the polygon
-  return odd;
+    }
+    //If the number of crossings was odd, the point is in the polygon
+    return odd;
 };
 
-InitMassPoint(20,0,0.3);
-InitMassPoint(70,100,0.2);
-InitMassPoint(120,50,0.4);
-InitPolygon([[10,700],[600,700],[600,760],[10,760]]);
+//InitMassPoint(positionX = 70, positionY = 0, bounce = 0.2);
+//InitMassPoint(positionX = 120, positionY = 0, bounce = 0.35);
+for(let row = 0; row < 5; row++){
+  for(let column = 1; column < 6; column++){
+    InitMassPoint(positionX = column * 20, positionY = row * 20, bounce = 0.3);
+  }
+}
+//var spring = new Spring(mp1, mp2, 100, 10, 10);
+InitPolygon([[0, canvas.height - 10], [canvas.width, canvas.height - 10], [canvas.width, canvas.height + 100], [0, canvas.height + 100]]);
 
-document.onclick=(e)=>{
-  InitMassPoint(e.x,e.y,0.5)
+setInterval(TickGravity, 10);
+document.onclick = (e) =>{
+  InitMassPoint(e.x,e.y,1)
 }
-function UpdateCounters(){
-  document.getElementById('numballs').innerHTML = massPoints.length.toString()
-}
-setInterval(TickGravity,10);
-setInterval(UpdateCounters,100);
